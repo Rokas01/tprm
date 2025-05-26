586f460ac6c402c7cb874dbb5c7e181b436daa5a
Rokas <rokas.deveikis@gmail.com>
2025-05-26 11:47:21 +0000

    pptx update

--- pptx_generator.py
+++ pptx_generator.py
@@ -1,4 +1,14 @@
 from pptx import Presentation
+from pptx.util import Pt
+
+def apply_font(text_object):
+    font = text_object.font
+    font.name = 'Calibri'
+    font.size = Pt(8)
+    #font.bold = True
+    #font.italic = None  # cause value to be inherited from theme
+
+
 
 
 def add_slide(prs_object_input, layout_input, title_input):
@@ -16,26 +26,40 @@ def add_slide(prs_object_input, layout_input, title_input):
     #title_shape = shapes.title
     body_shape = shapes.placeholders[1]
     tf = body_shape.text_frame
-    tf.text = 'Requirement'
+    #tf.text = 'Requirement'
+    #tf.font.size = Pt(12)
+    #tf.font.bold = True
+
+    p0 = tf.add_paragraph()
+    p0.text = 'Requirement'
+    p0.font.size = Pt(12)
+    p0.font.bold = True
 
     p1 = tf.add_paragraph()
     p1.text = requirement_text_from_call
+    p1.font.size = Pt(9)
     p1.level = 1
 
     p2 = tf.add_paragraph()
     p2.text = "Summary"
     p2.level = 0
+    p2.font.size = Pt(12)
+    p2.font.bold = True
 
     p3 = tf.add_paragraph()
     p3.text = summary_text_from_call
+    p3.font.size = Pt(9)
     p3.level = 1
 
     p4 = tf.add_paragraph()
     p4.text = "Emerging observations"
+    p4.font.size = Pt(12)
+    p4.font.bold = True
     p4.level = 0
 
     p5 = tf.add_paragraph()
     p5.text = observation_text_from_call
+    p5.font.size = Pt(9)
     p5.level = 1
     
     return slide
@@ -61,8 +85,7 @@ def create_presentation_report_findings(title_text, subtitle_text, requirementsA
 
     prs.save('test2.pptx')
 
+test_results = [["Article 21", "The measures referred to in paragraph 1 shall be based", "this is a summary", "not requested"], ["Article 22", "test text for the next article", "this is a summary", "not requested"]]
 
-#test_results = [["Article 21", "The measures referred to in paragraph 1 shall be based", "this is a summary", "not requested"], ["Article 22", "test text for the next article", "this is a summary", "not requested"]]
-
-#create_presentation_report_findings("NIS2 compliance", "assessment report", test_results)
+create_presentation_report_findings("NIS2 compliance", "assessment report", test_results)
 
